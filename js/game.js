14ab46a866e12fcf7cb689438b7e73ef40942b62
General Assembly | Anthony Ng <generalassemblyanthonyng@Anthonys-MacBook-Air-4.local>
2015-10-14 17:53:01 -0400

    Created Modules for ScrollingLetters, GameObject, and KeyListener

---
+++ js/game.js
@@ -0,0 +1,105 @@
+/*
+  GameObject
+    -create new random strings
+    -match up typed letters against current letter
+*/
+
+var GameObject = (function() {
+  // private
+  var currentStringArray = [];
+  var letterSet = 'abcdefghijklmnopqrstuvwxyz';
+  var numberSet = '1234567890';
+  var charSet = letterSet + numberSet;
+  var arrayLength = 40;
+
+  // public
+  return {
+    // create new string array
+    createStringArray: function() {
+      // only create when currentStringArray is empty
+      if(currentStringArray.length === 0) {
+        for(var i = 0; i < arrayLength; i++) {
+          var randomNumber = Math.floor(Math.random() * charSet.length);
+          currentStringArray.push(charSet[randomNumber]);
+        };
+      };
+    },
+
+    // getter for currentStringArray
+    getStringArray: function() {
+      return currentStringArray;
+    },
+
+    // check for match with first letter of currentStringArray
+    checkForMatch: function(letter) {
+      return letter === currentStringArray[0] ? true : false;
+    },
+
+    // remove first value of array
+    removeFirstValue: function() {
+      currentStringArray = currentStringArray.splice(1);
+    }
+
+  }
+})();
+
+/*
+  LetterScroller
+    -scroll all of the letters through the game window
+*/
+
+var LetterScroller = (function() {
+
+  var currentStringArray = [];
+
+  return {
+    createStringArray: function(array) {
+      if(currentStringArray.length === 0) {
+        currentStringArray = array;
+      };
+    },
+
+    getStringArray: function() {
+      return currentStringArray;
+    },
+
+    scrollNewLetter: function() {
+      // create a new letter
+      var newLetter = $('<div>', {id:currentStringArray[0], class:'scrollingLetter'});
+      newLetter.html(currentStringArray[0]);
+      newLetter.css({top:$(window).height() - 200, left:$(window).width()});
+
+      // add a scroll event to the letter
+      newLetter.animate({left:'-1000px'}, {duration:6000});
+      $('.scrollingLetterContainer').append(newLetter);
+
+      // remove the old letter
+      currentStringArray = currentStringArray.splice(1);
+    },
+
+  }
+})();
+
+var KeyListener = (function() {
+  var lastKeyPress;
+
+  return {
+    // set up eventListener
+    setUp: function() {
+      $(document).keypress(function(e) {
+        KeyListener.processKeyPress(String.fromCharCode(e.keyCode));
+      });
+    },
+
+    // update lastKeyPress
+    processKeyPress: function(keyPress) {
+      console.log(keyPress + ' was pressed.');
+      lastKeyPress = keyPress;
+    },
+
+    getLastKeyPress: function() {
+      return lastKeyPress;
+    }
+
+  }
+})();
